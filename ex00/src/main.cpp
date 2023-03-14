58fafa6f85e61717e83a924b67e00a72591c3af3
jleroux <jleroux>
2023-03-14 17:14:39 +0100

    Start adding failsafe checking

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,34 +6,60 @@
 /*   By: jleroux <jleroux@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 15:50:15 by jleroux           #+#    #+#             */
-/*   Updated: 2023/03/14 16:43:34 by jleroux          ###   ########.fr       */
+/*   Updated: 2023/03/14 17:14:20 by jleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <utility>
+
+//Error: not a positive number.
+//Error: too large a number.
+string_to_positive_float(std::string str)
+{
+	float	result;
+
+	result = strtof(str); //Does this throw if too big ?
+	if (result < 0)
+		throw negativeNumber();
+	return result;
+}
+
+//Error: bad input => 2001-42-42
+std::tm	string_to_date(std::string str)
+{
+	std::string	split;
+    std::tm		tm{};
+
+	split[] = str.split('-'); //need protection
+
+	if (split.size() != 3)
+		throw badInput(str);
+
+	//Does this throw if bad date inputed ?
+	tm.tm_year = strtol(split[0]) - 1900;
+	tm.tm_mon = strtol(split[1]);
+    tm.tm_mday = strtol(split[2]);
+
+	return tm;
+}
 
 //format: "date | amount" or "date | rate"
-//Error: not a positive number.
-//Error: bad input => 2001-42-42
-//Error: too large a number.
-
 std::pair<date_t, float>	get_pair(std::string line)
 {
 	std::pair<std::string, std::string>	raw;
-	std::pair<date_t, float>				parsed;
+	std::pair<date_t, float>			parsed;
 
-	raw = line.split('|');
-	date = string_to_date(raw.first);
-	amount = string_to_positive_float(raw.second);
-	check_valid_date(line);
-	check_valid_value(line);
-	return std::make_pair<date_t, float>(date, amount);
+	raw = line.split('|');	//need protection
+	parsed.first = string_to_date(raw.first);
+	parsed.second = string_to_positive_float(raw.second);
+	return parsed;
 }
 
 
 std::map<date_t, float>	parse(FILE file) //inputfilestream
 {
-	std::map<date_t, float>	map;
+	std::map<date_t, float>		map;
 	std::pair<date_t, float>	pair;
 
 	for (each line in file)
